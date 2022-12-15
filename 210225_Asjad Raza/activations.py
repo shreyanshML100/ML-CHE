bd48a7e3d9279b2bf5986bd07fb9b0246643b99a
asjadr21 <98120378+asjadr21@users.noreply.github.com>
2022-12-15 18:42:01 +0530

    Delete activations.py

--- 210225_Asjad Raza/activations.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib as plt
-
-def sigmoid(z):
-    return 1/(1+np.exp(-z))
-def tanh(Z):
-    return (np.exp(Z)-np.exp(-Z))/(np.exp(Z)+(np.exp(-Z)))
-def Relu(Z):
-    return (np.maximum(0,Z))
