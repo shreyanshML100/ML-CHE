40f1d040536a675ad7ea01d99fac68c5f5c7ecae
Aman Kumar <109408666+amaniitk@users.noreply.github.com>
2022-12-16 13:20:03 +0530

    Update differentiate.py

--- 210110_Aman Kumar/Assignment_0/Assignment_0/differentiate.py
+++ 210110_Aman Kumar/Assignment_0/Assignment_0/differentiate.py
@@ -5,13 +5,11 @@ def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 def diff_sig(sigmoid,z0,h):
     return (sigmoid(z0+h) - sigmoid(z0-h))/(2*h)
-print(diff_sig(sigmoid,0,1))
 
 def tanh(z):
     return (np.exp(z)-np.exp(-z))/(np.exp(z)+np.exp(-z))
 def diff_tan(tanh,z0,h):
     return (tanh(z0+h) - tanh(z0-h))/(2*h)
-print(diff_tan(tanh,0,1))
 
 def ReLU(z):
     if (z>0):
@@ -20,4 +18,4 @@ def ReLU(z):
         return 0
 def diff_rel(ReLU,z0,h):
     return (ReLU(z0+h) - ReLU(z0-h))/(2*h)
-print(diff_rel(ReLU,0,1))
+
