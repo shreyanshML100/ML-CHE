9c28b4e9cf1066f75428f62c252dd04cb5b25153
asjadr21 <98120378+asjadr21@users.noreply.github.com>
2022-12-17 15:10:47 +0530

    Update differentiate.py

--- 210225_Asjad Raza/Assignment_0/differentiate.py
+++ 210225_Asjad Raza/Assignment_0/differentiate.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import activations as act
 
 def sig_deriv(z):
-    return -np.exp(-z)/(1+np.exp(-z))^2
+    return -np.exp(-z)/(1+np.exp(-z))**2
 def tanh_deriv(z):
     return 1+ act.tanh(z)
 def ReLU_deriv(z):
