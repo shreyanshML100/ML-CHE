26edeeb44192411fd64de0794cacb0e1475ae094
Bhanu1506 <118839172+Bhanu1506@users.noreply.github.com>
2022-12-17 12:30:19 +0530

    Add files via upload

---
+++ 210263_Bhanupratap/Assignnment_0/differentiate.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import activation as act
+
+def sigmoidDerivative(z):
+    return (-np.exp(-z)/(1+np.exp(-z))^2)
+def tanhDerivative(z):
+    return (1+ act.tanh(z))
+def ReLUDerivative(z):
+    return ((z>0)*1)
+
+
+
