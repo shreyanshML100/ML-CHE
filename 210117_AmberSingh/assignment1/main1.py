b6db361d1e8ee61dad076ea850e2070eb848d1d1
Amber404 <95181561+Amber404@users.noreply.github.com>
2022-12-26 01:35:20 +0530

    Add files via upload

--- 210117_AmberSingh/assignment1/main1.py
+++ 210117_AmberSingh/assignment1/main1.py
@@ -2,9 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import linearRegression as ok
 
+X_train = np.array([2.5, 4.7, 5.2, 7.3, 9.5, 11.5])
+Y_train = np.array([5.21, 7.70, 8.30, 11, 14.5, 15])
+plt.scatter(X_train,Y_train)
+plt.show()
 X_train = np.matrix([2.5, 4.7, 5.2, 7.3, 9.5, 11.5])
-X_train = np.transpose(X_train)
 Y_train = np.matrix([5.21, 7.70, 8.30, 11, 14.5, 15])
+
+X_train = np.transpose(X_train)
 Y_train = np.transpose(Y_train)
 theta = np.matrix([0])
 alpha=0.01
@@ -14,3 +19,4 @@ NumberofIterations=75
 print(ok.costFun(theta, X_train, Y_train))
 print(ok.fitNormal(X_train,Y_train))
 print(ok.fitGD(X_train, Y_train, alpha, lam,type_, NumberofIterations))
+
