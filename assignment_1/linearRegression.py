fdf1bc5e013b4d4442f5fd592b7f641df9e0b0ca
ADiti16Sengar <110708770+ADiti16Sengar@users.noreply.github.com>
2022-12-25 20:11:12 +0530

    Add files via upload

--- assignment_1/linearRegression.py
+++ assignment_1/linearRegression.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 def costFun(theta,X_train,Y_train):
     m=len(X_train)
@@ -11,66 +11,104 @@ def diffcost(theta,X_train,Y_train):
 
 
 def fitGD(X_train,Y_train,alpha,lamda,regu,itr):
-   
      m = np.shape(X_train)[0]  # total number of samples
      n = np.shape(X_train)[1]  # total number of features
     
      X_train = np.concatenate((np.ones((m, 1)), X_train), axis=1)
-     W = np.random.randn(n + 1, )
+     theta = np.random.randn(n + 1, )
      
      # stores the updates on the cost function (loss function)
      cost_history_list = []
-   
+     iteration=[]
      # iterate until the maximum number of iterations
      for current_iteration in range(itr):  # begin the process
  
         # compute the dot product between our feature 'X' and weight 'W'
-        y_estimated = X_train.dot(W)
+        y_estimated = X_train.dot(theta)
  
         # calculate the difference between the actual and predicted value
         error = y_estimated - Y_train
  
         # regularization term
+        reg_term=0
+        gradient = (1 / m)*( X_train.T.dot(error))
         if regu==1:
-           ridge_reg_term = (lambda/(2 * m)) * np.sum(np.square(W))
-           gradient = (1 / m)*( X.T.dot(error) + (lambda* W))
-           # Now we have to update our weights
-           W = W - alpha * gradient
+           reg_term =(lamda/(2 * m)) * np.sum(np.square(theta))
+           gradient = (1 / m)*( X_train.T.dot(error) + (lamda* theta))
         if regu==2:
-           ridge_reg_term = (lambda/(2 * m)) * np.sum(np.absolute(W))
+            penalty_term_diff = np.random.randn(n + 1, )
+            reg_term = (lamda/(2 * m)) * np.sum(np.absolute(theta))
+            for i in range(n+1):
+             if theta[i]>0:
+              penalty_term_diff[i]=(lamda)/m
+             else:
+              penalty_term_diff[i]=(-lamda)/m
+            gradient = (1 / m)*( X_train.T.dot(error)) +penalty_term_diff
         if regu==3:
-           ridge_reg_term= (lambda /(2 * m)) *(alpha*np.sum(np.absolute(W))+(1-alpha)*np.sum(np.square(W)))
+             reg_term= (lamda/(2 * m)) *(alpha*np.sum(np.absolute(theta))+(1-alpha)*np.sum(np.square(theta)))
+             penalty_term_diff = np.random.randn(n + 1, )
+             for i in range(n+1):
+              if theta[i]>0:
+               penalty_term_diff[i]=(lamda)/m
+              else:
+               penalty_term_diff[i]=(-lamda)/m
+             gradient = (1 / m)*( X_train.T.dot(error) + (lamda*(1-alpha)*theta))+alpha* penalty_term_diff
+
+        # Now we have to update our weights
+        theta = theta - alpha * gradient
         # calculate the cost (MSE) + regularization term
-        cost = (1 / 2 * m) * np.sum(error ** 2) + ridge_reg_term
-        print(f"cost:{cost}   iteration: {current_iteration}")
+        cost = (1 / 2 * m) * np.sum(error ** 2) + reg_term
+        #print(f"cost:{cost}   iteration: {current_iteration}")
         cost_history_list.append(cost)
-     return W
+        iteration.append(current_iteration)
+     
+     plt.plot(iteration,cost_history_list,color="green")
+     
+     return theta
+
 
 def fitNormal(X_train,Y_train):
     theta=np.linalg.inv(X_train.T.dot(X_train)).dot(X_train.T).dot(Y_train)
     return theta
 
+global x
 
 def locallyWeighted(X_train,Y_train,x,tau):
     
-        x = np.mat([x, 1])
-        m = np.shape(X_train)[0]
+        x = np.r_[1,x]
         X_train = np.c_[np.ones(len(X_train)), X_train]
-        W = np.mat(np.eye(m))
-        for i in range(m):
-          xi = X_train[i]
-          denominator = (-2 * tau * tau)
-          W[i, i] = np.exp(np.dot((xi-x), (xi-x).T)/denominator)
-        theta = np.linalg.pinv(X_train.T*(W * X_train))*(X_train.T*(W * Y_train))
-        y = x@theta
+        m = np.shape(X_train)[0]
+        y= np.zeros(m)
+        xw = X_train.T *( np.exp(np.sum((X_train - x) ** 2, axis=1) / (-2 * (tau **2) )))
+        theta = np.linalg.pinv(xw @ X_train) @ xw @ Y_train
+        y=x@theta
         return y
-   """ x = np.r_[1, x]
-    X_train = np.c_[np.ones(len(X_train)), X_train]
-     
-    # fit model: normal equations with kernel
-    xw = X_train.T * np.exp(np.sum((X - x0) ** 2, axis=1) / (-2 * (tau **2) ))
-    theta = np.linalg.pinv(xw @ X) @ xw @ Y
-    # "@" is used to
-    # predict value
-    return x0 @ theta"""
+
+
+"""
+Y_train=np.array([5.21,7.7,8.3,11,14.5,15])
+X_train=np.array([2.5,4.7,5.2,7.3,9.5,11.5])
+tau=np.array([0.01,0.1,1,10,100])
+
+X_test=np.array([3.5,5,6,8,10])
+for i in tau:
+    Y=[]
+    for x in X_test:
+      Y.append(locallyWeighted(X_train,Y_train,x,i))
+    print(Y)
+  """
+"""
+X_train=np.array([[2.5],[4.7],[5.2],[7.3],[9.5],[11.5]])
+#X_train = np.concatenate((np.ones((6, 1)), X_train), axis=1)
+print(X_train)
+#print(np.shape(X_train))
+n=np.shape(X_train)[1]
+print(n)
+Y_train=np.array([5.21,7.7,8.3,11,14.5,15])
+#Y_train=np.array([[5.21],[7.7],[8.3],[11],[14.5],[15]])
+fitGD(X_train,Y_train,0.001,1,1,100)
+fitGD(X_train,Y_train,0.001,1,2,100)
+plt.show()
+print("n")"""
+
 
